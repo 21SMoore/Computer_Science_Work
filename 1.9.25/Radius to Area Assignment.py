2e2890590db2a3d58471c4d0015ca70cbf3f8f36
21SMoore <21SMoore.ACC@lwetb.ie>
2025-09-12 09:41:16 +0100

    Update Radius to Area Assignment.py

--- 1.9.25/Radius to Area Assignment.py
+++ 1.9.25/Radius to Area Assignment.py
@@ -3,6 +3,7 @@
 #6. Write a python program that accepts radius of a circle and prints its area.
 
 radius = input('Enter a radius: ')
-radius = int(radius)
+radius = float(radius)
 radius_squared = (radius * radius)
 print('The area is:',3.14 * radius_squared)
+
